9985bd15f94a713381b0fa5abc43ea4994ba155e
André Davis <CubicleJockey@users.noreply.github.com>
2023-07-12 10:07:28 -0700

    Going back to work work.

--- tests/Jotform.Tests/Endpoints/Folder/DeleteFolderTests.cs
+++ tests/Jotform.Tests/Endpoints/Folder/DeleteFolderTests.cs
@@ -1,7 +1,17 @@
 ﻿namespace Jotform.Tests.Endpoints.Folder;
 
 [TestClass]
-public class DeleteFolderTests
+public class DeleteFolderTests : BaseTestEndpoint
 {
+    public DeleteFolderTests()
+    {
+        A.CallTo(async () => await fakeHttpClient.DeleteAsync($"{fakeHttpClient.BaseAddress}folder/1"))
 
+    }
+
+    [TestMethod]
+    public async Task DeleteFolder_Success()
+    {
+
+    }
 }
